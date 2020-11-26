b31fb7660992d3140f5fc9bfa4d1233813d0dfb9
Danny Gershman <danny.gershman@gmail.com>
2020-11-26 18:19:29 -0500

    adding script for gh actions

---
+++ gh-release-notes.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# if travis isn't calling the script we abort
+if [[ -z "$CI" ]]; then
+    echo "Script is only to be run by Github ACtions" 1>&2
+    exit 1
+fi
+
+# if tag isn't set we abort
+if [[ -z "$GITHUB_REF" ]]; then
+    echo "Tag not set, changelog export." 1>&2
+    exit 0
+fi
+
+# if required params are not set we abort
+if [[ -z "$1" ]] || [[ -z "$2" ]]; then
+    echo "File or Delimiter not set, aborting changelog export." 1>&2
+    exit 0
+fi
+
+if [[ "$2" == *"wp"* ]]; then
+  CHANGE=$(sed -e '1,/= Changelog =/d' $1 | sed "/$GITHUB_REF/,/=/!d;//d" | awk 'NF')
+else
+   CHANGE=$(sed "/$GITHUB_REF/,/$2/!d;//d" $1 | awk 'NF')
+fi
+
+echo "$CHANGE" > changelog.txt
