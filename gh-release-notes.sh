cc1d8f607a2203b0cfa5af3cbbf530841af5ee06
Danny Gershman <danny.gershman@gmail.com>
2020-11-27 17:21:45 -0500

    setting tag correct

--- gh-release-notes.sh
+++ gh-release-notes.sh
@@ -2,7 +2,7 @@
 
 # if travis isn't calling the script we abort
 if [[ -z "$CI" ]]; then
-    echo "Script is only to be run by Github ACtions" 1>&2
+    echo "Script is only to be run by Github Actions" 1>&2
     exit 1
 fi
 
@@ -12,6 +12,8 @@ if [[ -z "$GITHUB_REF" ]]; then
     exit 0
 fi
 
+$TAG="${GITHUB_REF##*/}"
+
 # if required params are not set we abort
 if [[ -z "$1" ]] || [[ -z "$2" ]]; then
     echo "File or Delimiter not set, aborting changelog export." 1>&2
@@ -19,9 +21,9 @@ if [[ -z "$1" ]] || [[ -z "$2" ]]; then
 fi
 
 if [[ "$2" == *"wp"* ]]; then
-  CHANGE=$(sed -e '1,/= Changelog =/d' $1 | sed "/$GITHUB_REF/,/=/!d;//d" | awk 'NF')
+  CHANGE=$(sed -e '1,/= Changelog =/d' $1 | sed "/$TAG/,/=/!d;//d" | awk 'NF')
 else
-   CHANGE=$(sed "/$GITHUB_REF/,/$2/!d;//d" $1 | awk 'NF')
+   CHANGE=$(sed "/$TAG/,/$2/!d;//d" $1 | awk 'NF')
 fi
 
 echo "$CHANGE" > changelog.txt
