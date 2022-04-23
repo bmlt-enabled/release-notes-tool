7b1d7c3f3d78058e50854ffef915e38d962e740f
Patrick Joyce <pjaudiomv@gmail.com>
2022-04-22 23:48:59 -0400

    allow running with gha

--- release-notes.sh
+++ release-notes.sh
@@ -2,14 +2,18 @@
 
 # TRAVIS_TAG=3.6.1
 
-# if travis isn't calling the script we abort
-if [[ -z "$TRAVIS" ]]; then
-    echo "Script is only to be run by Travis CI" 1>&2
+# if CI isn't calling the script we abort
+if [[ -n "$TRAVIS" ]]; then
+    SCRIPT_TAG=$TRAVIS_TAG
+elif [[ -n "$GITHUB_WORKFLOW" ]]; then
+    SCRIPT_TAG=$GITHUB_REF
+else
+    echo "Script is only to be run by Travis CI or GitHub Actions" 1>&2
     exit 1
 fi
 
 # if tag isn't set we abort
-if [[ -z "$TRAVIS_TAG" ]]; then
+if [[ -z "$SCRIPT_TAG" ]]; then
     echo "Tag not set, changelog export." 1>&2
     exit 0
 fi
@@ -21,9 +25,9 @@ if [[ -z "$1" ]] || [[ -z "$2" ]]; then
 fi
 
 if [[ "$2" == *"wp"* ]]; then
-  CHANGE=$(sed -e '1,/= Changelog =/d' $1 | sed "/$TRAVIS_TAG/,/=/!d;//d" | awk 'NF')
+  CHANGE=$(sed -e '1,/= Changelog =/d' $1 | sed "/$SCRIPT_TAG/,/=/!d;//d" | awk 'NF')
 else
-   CHANGE=$(sed "/$TRAVIS_TAG/,/$2/!d;//d" $1 | awk 'NF')
+   CHANGE=$(sed "/$SCRIPT_TAG/,/$2/!d;//d" $1 | awk 'NF')
 fi
 
 echo "$CHANGE" > changelog.txt
